46334a4af9d3443c53fa15aa9bf33658db0e3d4f
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-08-31 06:50:11 +0100

    first commit for app

---
+++ app/_layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const app = ()=> {
+    return (
+        <View>
+            <Text>Welcome</Text>
+            <Text>Welcome</Text>
+            <Text>Welcome</Text>
+            <Text>Welcome</Text>
+            <Text>Welcome</Text>
+        </View>
+    )
+}
+export default app;
