65dae610c402dbb068952e2950724ba3781f71d6
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-09-01 23:48:40 +0100

    added mine

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
 
-const app = ()=> {
+const firsst = () => {
     return (
-        <View>
-            <Text>Welcome</Text>
-            <Text>Welcome</Text>
-            <Text>Welcome</Text>
-            <Text>Welcome</Text>
-            <Text>Welcome</Text>
+        <View style={{
+            marginLeft: 50,
+        }}>
+            <Text style={{
+                color: 'green',
+                height: 100,
+                marginTop: 40,
+                marginLeft: 20,
+            }}>Hi, First app coming</Text>
+            <Text>Favour is the best mobile developer tutor</Text>
+
+            <View style={{
+                marginLeft:40,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: 30,
+                backgroundColor: 'yellow',
+                width: 100,
+                height: 100,
+            }}>
+                <Text>Thanks</Text>
+                <Text>A</Text>
+                <Text>Lot</Text>
+                <Text>Sir</Text>
+            </View>
+
         </View>
-    )
-}
-export default app;
+    );
+
+
+};
+export default firsst;
